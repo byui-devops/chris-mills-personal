7e611677257e95ece0668523446fb19e879c0f22
cplmills <91394219+cplmills@users.noreply.github.com>
2026-03-19 13:31:57 +1000

    Add terraform release workflow

---
+++ bulletin-board-app/iac/main.tf
@@ -0,0 +1,62 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_security_group" "app_sg" {
+  name = "app_sg"
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y git nodejs npm
+
+              cd /home/ubuntu
+              git clone https://github.com/byui-devops/chris-mills-personal.git
+              cd chris-mills-personal/bulletin-board-app/app
+
+              npm install
+              npm start
+              EOF
+
+  tags = {
+    Name = "bulletin-board-app"
+  }
+}
